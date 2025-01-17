f8b07f6251c23cb00371e26d0ff752c10b1e8b4c
y1220 <ga.2.y1220@gmail.com>
2025-01-17 17:53:08 +0100

    adjust the prompt

--- mindguide/src/services/LLM/llm_model.js
+++ mindguide/src/services/LLM/llm_model.js
@@ -9,9 +9,6 @@ const client = new OpenAI({
 
 let conversationHistory = [];
 
-
-
-
 const role_temperatures = {
   moderator: 0.2,
   empathic: 0.3,
@@ -19,15 +16,31 @@ const role_temperatures = {
 };
 
 // Function to get response with initial instruction only once
-export async function getOpenAIResponse(newPrompt, modelType, participantsNumber, names) {
-
+export async function getOpenAIResponse(
+  newPrompt,
+  modelType,
+  participantsNumber,
+  names
+) {
   let role_behaviors = {
     moderator:
-    "For the rest of the conversation you are Emily, the moderator in a group therapy session with " + participantsNumber + " other participants ( " +names+ " ).\nYour only tasks are:\n- Make sure that all participants interact with each other as a group.\n- Make sure the conversation goes on properly.\n\nYour responses must be very short and foster group dynamics and help all participants to have the opportunity to express their thoughts.\nYou must not give your personal opinions or suggestions to participants.\nMake sure everyone introduces themselves by their name at first.\nIntervene only if you feel it is strictly necessary.", 
+      "For the rest of the conversation you are Emily, the moderator in a group therapy session with " +
+      participantsNumber +
+      " other participants ( " +
+      names +
+      " ).\nYour only tasks are:\n- Make sure that all participants interact with each other as a group.\n- Make sure the conversation goes on properly.\n\nYour responses must be very short and foster group dynamics and help all participants to have the opportunity to express their thoughts.\nYou must not give your personal opinions or suggestions to participants.\nMake sure everyone introduces themselves by their name at first.\nIntervene only if you feel it is strictly necessary.",
     empathic:
-      "For the rest of the conversation you are Emily, an empathic listener who listens to the problems of " + participantsNumber + " other participants ( " +names+ " ) in the conversation.\nYour tasks are:\n- Allow participants to talk as much as possible to vent.\n- Make sure everyone is talking.\n- Help participants interact with each other.\n\nYou must interact with an understanding, calm and empathetic attitude, making participants feel heard, understood and in a safe environment.\nYou must keep your answers very brief and must not give suggestions or personal opinions.\nMake sure everyone introduces themselves by their name at first.\nDo not interrupt the participants if they are speaking.",
+      "For the rest of the conversation you are Emily, an empathic listener who listens to the problems of " +
+      participantsNumber +
+      " other participants ( " +
+      names +
+      " ) in the conversation.\nYour tasks are:\n- Allow participants to talk as much as possible to vent.\n- Make sure everyone is talking.\n- Help participants interact with each other.\n\nYou must interact with an understanding, calm and empathetic attitude, making participants feel heard, understood and in a safe environment.\nYou must keep your answers very brief and must not give suggestions or personal opinions.\nMake sure everyone introduces themselves by their name at first.\nDo not interrupt the participants if they are speaking.",
     peer:
-      "For the rest of the conversation you are Emily, a participant within a conversation with your " + participantsNumber + " friends ( " +names+ " ) who are discussing their problems.\nYour only tasks are:\n-Make sure everyone has a chance to talk and interact with each other\n- Make sure the conversation goes on properly.\n\nYou have to interact as one of their peers, so be friendly and inclined to help them.\nMake sure everyone introduces themselves by their name at first.",
+      "For the rest of the conversation you are Emily, a participant within a conversation with your " +
+      participantsNumber +
+      " friends ( " +
+      names +
+      " ) who are discussing their problems.\nYour only tasks are:\n-Make sure everyone has a chance to talk and interact with each other\n- Make sure the conversation goes on properly.\n\nYou have to interact as one of their peers, so be friendly and inclined to help them.\nMake sure everyone introduces themselves by their name at first.",
   };
 
   modelType = modelType || "moderator";
@@ -35,14 +48,17 @@ export async function getOpenAIResponse(newPrompt, modelType, participantsNumber
     role: "system",
     content:
       role_behaviors[modelType] +
-      "YOU MUST SET \"intervene\" to false EXCEPT ONLY ON THESE SITUATIONS:\n"+
-      "- When you are starting the session.\n"+
-      "- If someone is using inappropriate language and insulting\n"+
-      "-  If a participant is not allowing others to speak or a participant has been inactive for a while.\n"+
-      "-  If the discussion is going in circles and no progress is being made.\n\n"+
-      "Always respond in JSON format. When you don't want to intervene send the following: {\"response\":\"\",\"intervene\":false}. Otherwise when you need to talk put your dialog in the \"response\" field and set \"intervene\" to true. When the conversation is ending, say goodbye and thank the participants. Start the session after receiving this message.",
+      'YOU MUST SET "intervene" to false EXCEPT ONLY ON THESE SITUATIONS:\n' +
+      "- When you are starting the session.\n" +
+      "- If someone is using inappropriate language and insulting\n" +
+      "-  If a participant is not allowing others to speak or a participant has been inactive for a while.\n" +
+      "-  If the discussion is going in circles and no progress is being made.\n\n" +
+      'Always respond in JSON format. When you don\'t want to intervene send the following: {"response":"","intervene":false}. ' +
+      "Please keep in mind that sending intervene as false can be done only 3 consecutive times as maximun." +
+      'Otherwise when you need to talk put your dialog in the "response" field and set "intervene" to true. When the conversation is ending, ' +
+      "say goodbye and thank the participants. Start the session after receiving this message.",
   };
-  
+
   const result = await client.chat.completions.create({
     messages: [
       systemPrompt,
@@ -59,18 +75,21 @@ export async function getOpenAIResponse(newPrompt, modelType, participantsNumber
     content: newPrompt,
   });
 
-  console.log("LLM response:", result.choices[0].message.content)
+  console.log("LLM response:", result.choices[0].message.content);
   let response = "";
   try {
     response = JSON.parse(result.choices[0].message.content); // Get the response from the result
   } catch (error) {
-    const cleanedResponse = result.choices[0].message.content.replace("response:", "");
+    const cleanedResponse = result.choices[0].message.content.replace(
+      "response:",
+      ""
+    );
     response = { response: cleanedResponse, intervene: true };
     console.log("Error: ", error);
   }
 
   if (!response.response) {
-    response.response = ""
+    response.response = "";
   }
 
   conversationHistory.push({
@@ -78,10 +97,10 @@ export async function getOpenAIResponse(newPrompt, modelType, participantsNumber
     content: JSON.stringify(response),
   }); // Add the response to the history
 
-  if (conversationHistory.length > 10) {
-    conversationHistory.shift(); // Remove the oldest moderator response
-    conversationHistory.shift(); // Remove the oldest user message
-  }
+  // if (conversationHistory.length > 10) {
+  //   conversationHistory.shift(); // Remove the oldest moderator response
+  //   conversationHistory.shift(); // Remove the oldest user message
+  // }
 
   if (response.intervene) {
     return response.response;
@@ -90,8 +109,18 @@ export async function getOpenAIResponse(newPrompt, modelType, participantsNumber
   return "";
 }
 
-export async function getModeratorResponse(speechText, modelType, numberParticipants, names) {
-  const response = await getOpenAIResponse(speechText, modelType, numberParticipants, names);
+export async function getModeratorResponse(
+  speechText,
+  modelType,
+  numberParticipants,
+  names
+) {
+  const response = await getOpenAIResponse(
+    speechText,
+    modelType,
+    numberParticipants,
+    names
+  );
   log.info("Moderator: ", response);
 
   return response;
