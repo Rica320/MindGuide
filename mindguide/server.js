e27cab2af27ddfec669a05a2c18b049403350ddc
y1220 <ga.2.y1220@gmail.com>
2025-01-29 21:36:16 +0100

    remove outdated reference

--- mindguide/server.js
+++ mindguide/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-const path = require("path");
 const winston = require("winston");
 const cors = require("cors");
 
